3347dcea4164366ca4804de5d082d6885701ac96
Mila-Kostic <milakostic.eng@gmail.com>
2022-04-09 17:25:20 +0200

    scopeVar

---
+++ scopeVar.js
@@ -0,0 +1,16 @@
+var g = 10; // global var
+
+
+function info(){
+    var l = 20; //local var
+    console.log(l);
+}
+
+console.log(info());
+ 
+if() {
+    var name = "Code"; // global var
+}
+
+console.log(g);
+console.log(name);
